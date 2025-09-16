0210044175927e24c90859260a2dd1aa5ea8c9a2
Alex Bevilacqua <alex@alexbevi.com>
2025-09-16 15:16:01 -0400

    Refactor MongoDB client initialization logic
    Comment out development mode logic for MongoDB client initialization.

--- mongodb.ts
+++ mongodb.ts
@@ -9,24 +9,24 @@ const options: MongoClientOptions = {
 let client: MongoClient;
 
 if (uri) {
-  if (process.env.NODE_ENV === "development") {
-    // In development mode, use a global variable so that the value
-    // is preserved across module reloads caused by HMR (Hot Module Replacement).
-    const globalWithMongo = global as typeof globalThis & {
-      _mongoClient?: MongoClient;
-    };
+  // if (process.env.NODE_ENV === "development") {
+  //   // In development mode, use a global variable so that the value
+  //   // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  //   const globalWithMongo = global as typeof globalThis & {
+  //     _mongoClient?: MongoClient;
+  //   };
 
-    if (!globalWithMongo._mongoClient) {
-      client = new MongoClient(uri, options);
-      globalWithMongo._mongoClient = client;
-    }
-  } else {
+  //   if (!globalWithMongo._mongoClient) {
+  //     client = new MongoClient(uri, options);
+  //     globalWithMongo._mongoClient = client;
+  //   }
+  // } else {
     // In production mode, it's best to not use a global variable.
     client = new MongoClient(uri, options);
 
     // Attach the client to ensure proper cleanup on function suspension
     attachDatabasePool(client);
-  }
+  // }
 }
 
 export async function withClient<T>(
