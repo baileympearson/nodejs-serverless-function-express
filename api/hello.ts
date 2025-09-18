7d86c890d29b907a639c358850d3d99a514d4c66
bailey <bailey.pearson@mongodb.com>
2025-09-18 12:41:04 -0600

    add a retry

--- api/hello.ts
+++ api/hello.ts
@@ -1,7 +1,7 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { withClient } from "../mongodb";
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+async function handle(req: VercelRequest, res: VercelResponse, attempt: number = 0) {
   try {
     const documents = await withClient(async (client) => {
       const db = client.db("sample_restaurants");
@@ -11,9 +11,15 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
     return res.json(documents);
   } catch (error) {
+    if (String(error).includes("tlsv1") && attempt < 1) {
+      return await handle(req, res, attempt + 1);
+    }
     console.error("Database connection error:", error);
     return res.status(500).json({
       error: "Failed to connect to database",
     });
   }
+}
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  return await  handle(req, res, 0);
 }
